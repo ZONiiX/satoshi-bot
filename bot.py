0cc453352056532c5aee6201aa6e0942a951a4f8
Tron Schell <zoniixyt@gmail.com>
2018-12-04 14:05:38 -0500

    rewrote the code, again.

--- bot.py
+++ bot.py
@@ -1,76 +1,73 @@
-import discord, json, requests, chalk
-from discord.ext import commands
-
-bot = commands.Bot(command_prefix="!", status=discord.Status.idle, activity=discord.game(name="Eating Fiat..."))
-
-bot.remove_command("help")
-
-@bot.event
-async def on_ready():
-    print(chalk.green("Ready!"))
-    print(chalk.blue(f"Serving: {len(bot.guilds)} guilds.")
-    await bot.change_presence(status=discord.Status.online, activity=discord.Game(nae="Fiat destroyed!"))
-
-
-@bot.command()
-async def satusd():
-    url = 'https://gntf7hd0uj.execute-api.us-east-2.amazonaws.com/default/satoshiAPI'
-    response = requests.get(url)
-    data = response.text
-    parsed = json.loads(data)
-    satusd = parsed['USD']
-
-    msg = "Satoshi price is : $" + format(satusd)
-    await client.send_message(message.channel, msg)
-
-@bot.command()
-async def sateur():
-    url = 'https://gntf7hd0uj.execute-api.us-east-2.amazonaws.com/default/satoshiAPI'
-    response = requests.get(url)
-    data = response.text
-    parsed = json.loads(data)
-    sateur = parsed['EUR']
-
-    msg = "Satoshi price is : €" + format(sateur)
-    await client.send_message(message.channel, msg)
-
-
-
-"""from discord import Game
-import discord
-import json
-import aiohttp
+import random
 import asyncio
+import aiohttp
+import json
 import requests
+from discord import Game
+from discord.ext.commands import Bot
 
-BOT_PREFIX = ('!')
-TOKEN = 'NTE5NTE3Mjg3MjA4NzE0MjQx.Dugd0w.y6PqkMeTNN5h3BfqhRj3MUKShDA'
 
-client = discord.Client()
+BOT_PREFIX = ("!")
+TOKEN = "NTE5NTE3Mjg3MjA4NzE0MjQx.Dugd0w.y6PqkMeTNN5h3BfqhRj3MUKShDA"  # Get at discordapp.com/developers/applications/me
+
+client = Bot(command_prefix=BOT_PREFIX)
+
+
+@client.command()
+async def satusd():
+    url = "https://gntf7hd0uj.execute-api.us-east-2.amazonaws.com/default/satoshiAPI"
+    response = requests.get(url)
+    data = response.text
+    parsed = json.loads(data)
+    usd_rate = parsed["USD"]
+
+    await client.say("1 Satoshi is: $" + usd_rate)
+
+@client.command()
+async def sateur():
+    url = "https://gntf7hd0uj.execute-api.us-east-2.amazonaws.com/default/satoshiAPI"
+    response = requests.get(url)
+    data = response.text
+    parsed = json.loads(data)
+    eur_rate = parsed["EUR"]
+    await client.say("1 Satoshi is: $" +eur_rate)
+
+
+@client.command()
+async def calcsatusd(number):
+    number = int(number)
+    url = "https://gntf7hd0uj.execute-api.us-east-2.amazonaws.com/default/satoshiAPI"
+    response = requests.get(url)
+    data = response.text
+    parsed = json.loads(data)
+    usd_rate = parsed["USD"]
+    final_price = usd_rate*number
+    await client.say(format(final_price), '.2f')
+
 @client.event
+async def on_read():
+    await client.change_presence(game=Game(name="with humans"))
+    print("Logged in as " + client.user.name)
 
-async def on_message(message):
-    if message.author == client.user:
-        return
 
-    if message.content.startswith('!satusd'):
-            url = 'https://gntf7hd0uj.execute-api.us-east-2.amazonaws.com/default/satoshiAPI'
-            response = requests.get(url)
-            data = response.text
-            parsed = json.loads(data)
-            satusd = parsed['USD']
+@client.command()
+async def bitcoin():
+    url = 'https://api.coindesk.com/v1/bpi/currentprice/BTC.json'
+    async with aiohttp.ClientSession() as session:  # Async HTTP request
+        raw_response = await session.get(url)
+        response = await raw_response.text()
+        response = json.loads(response)
+        await client.say("Bitcoin price is: $" + response['bpi']['USD']['rate'])
 
-            msg = "Satoshi price is : $" + format(satusd)
-            await client.send_message(message.channel, msg)
 
-    elif message.content.startswith('!sateur'):
-            url = 'https://gntf7hd0uj.execute-api.us-east-2.amazonaws.com/default/satoshiAPI'
-            response = requests.get(url)
-            data = response.text
-            parsed = json.loads(data)
-            sateur = parsed['EUR']
+async def list_servers():
+    await client.wait_until_ready()
+    while not client.is_closed:
+        print("Current servers:")
+        for server in client.servers:
+            print(server.name)
+        await asyncio.sleep(600)
 
-            msg = "Satoshi price is : €" + format(sateur)
-            await client.send_message(message.channel, msg)
 
-client.run(TOKEN)"""
+client.loop.create_task(list_servers())
+client.run(TOKEN)
