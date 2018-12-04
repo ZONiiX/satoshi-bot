c535eb178ef9f56a664af5c1398bbeccaf296c16
Tron Schell <zoniixyt@gmail.com>
2018-12-04 10:44:58 -0500

    rewritten all code for cleaner readability

--- bot.py
+++ bot.py
@@ -1,4 +1,42 @@
-from discord import Game
+import discord, json, requests, chalk
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix="!", status=discord.Status.idle, activity=discord.game(name="Eating Fiat..."))
+
+bot.remove_command("help")
+
+@bot.event
+async def on_ready():
+    print(chalk.green("Ready!"))
+    print(chalk.blue(f"Serving: {len(bot.guilds)} guilds.")
+    await bot.change_presence(status=discord.Status.online, activity=discord.Game(nae="Fiat destroyed!"))
+
+
+@bot.command()
+async def satusd():
+    url = 'https://gntf7hd0uj.execute-api.us-east-2.amazonaws.com/default/satoshiAPI'
+    response = requests.get(url)
+    data = response.text
+    parsed = json.loads(data)
+    satusd = parsed['USD']
+
+    msg = "Satoshi price is : $" + format(satusd)
+    await client.send_message(message.channel, msg)
+
+@bot.command()
+async def sateur():
+    url = 'https://gntf7hd0uj.execute-api.us-east-2.amazonaws.com/default/satoshiAPI'
+    response = requests.get(url)
+    data = response.text
+    parsed = json.loads(data)
+    sateur = parsed['EUR']
+
+    msg = "Satoshi price is : €" + format(sateur)
+    await client.send_message(message.channel, msg)
+
+
+
+"""from discord import Game
 import discord
 import json
 import aiohttp
@@ -35,4 +73,4 @@ async def on_message(message):
             msg = "Satoshi price is : €" + format(sateur)
             await client.send_message(message.channel, msg)
 
-client.run(TOKEN)
+client.run(TOKEN)"""
